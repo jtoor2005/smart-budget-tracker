439572016ceb3660cbdb01ea180d6fe5fe487ca6
Jaiveer Toor <jaiveertoor63@gmail.com>
2025-04-05 20:57:09 -0600

    Updated database.py file to include our new model.

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,19 @@
 import os
-from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Base, ExpenseModel, BudgetModel
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/expensedb")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
-class ExpenseModel(Base):
-    __tablename__ = "expenses"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    description = Column(String, index=True)
-    amount = Column(Float)
-    category = Column(String)
-    
 # Create tables
 Base.metadata.create_all(bind=engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
